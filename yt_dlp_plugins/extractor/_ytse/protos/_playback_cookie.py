2e13d5772512c4e14cfde715278ca0cce418597c
coletdjnz <coletdjnz@protonmail.com>
2024-11-05 21:06:01 +1300

    add some unknown fields to playback cookie

--- yt_dlp_plugins/extractor/_ytse/protos/_playback_cookie.py
+++ yt_dlp_plugins/extractor/_ytse/protos/_playback_cookie.py
@@ -7,5 +7,8 @@ from yt_dlp_plugins.extractor._ytse.protos._format_id import FormatId
 class PlaybackCookie:
     field1: typing.Optional[protobug.Int32] = protobug.field(1, default=None)
     field2: typing.Optional[protobug.Int32] = protobug.field(2, default=None)
+    field3: typing.Optional[protobug.Int32] = protobug.field(3, default=None)
+    field6: typing.Optional[protobug.Int32] = protobug.field(6, default=None)
     video_fmt: typing.Optional[FormatId] = protobug.field(7, default=None)
     audio_fmt: typing.Optional[FormatId] = protobug.field(8, default=None)
+    field14: typing.Optional[protobug.Int32] = protobug.field(14, default=None)
