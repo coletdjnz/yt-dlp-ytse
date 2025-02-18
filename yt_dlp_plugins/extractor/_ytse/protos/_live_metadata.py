a5324f335793e8455afba12d632edf8cc07265c9
coletdjnz <coletdjnz@protonmail.com>
2025-02-18 20:03:53 +1300

    fix(protos): LiveMetadata field optionality

--- yt_dlp_plugins/extractor/_ytse/protos/_live_metadata.py
+++ yt_dlp_plugins/extractor/_ytse/protos/_live_metadata.py
@@ -4,15 +4,15 @@ import protobug
 
 @protobug.message
 class LiveMetadata:
-    latest_sequence_number: typing.Optional[protobug.UInt32] = protobug.field(3)
-    latest_sequence_duration_ms: typing.Optional[protobug.UInt64] = protobug.field(4)
+    latest_sequence_number: typing.Optional[protobug.UInt32] = protobug.field(3, default=None)
+    latest_sequence_duration_ms: typing.Optional[protobug.UInt64] = protobug.field(4, default=None)
 
-    timestamp: typing.Optional[protobug.UInt64] = protobug.field(5)
-    unknown_field_10: typing.Optional[protobug.UInt32] = protobug.field(10)  # maybe live status?
+    timestamp: typing.Optional[protobug.UInt64] = protobug.field(5, default=None)
+    unknown_field_10: typing.Optional[protobug.UInt32] = protobug.field(10, default=None)  # maybe live status?
 
-    dvr_start_duration: typing.Optional[protobug.UInt64] = protobug.field(12)  # earliest you can rewind the livestream
-    dvr_start_timescale: typing.Optional[protobug.UInt32] = protobug.field(13)
+    dvr_start_duration: typing.Optional[protobug.UInt64] = protobug.field(12, default=None)  # earliest you can rewind the livestream
+    dvr_start_timescale: typing.Optional[protobug.UInt32] = protobug.field(13, default=None)
 
-    live_start_duration: typing.Optional[protobug.UInt64] = protobug.field(14)  # where SABR seek puts you to start streaming live?
-    live_start_timescale: typing.Optional[protobug.UInt32] = protobug.field(15)
+    live_start_duration: typing.Optional[protobug.UInt64] = protobug.field(14, default=None)  # where SABR seek puts you to start streaming live?
+    live_start_timescale: typing.Optional[protobug.UInt32] = protobug.field(15, default=None)
 
